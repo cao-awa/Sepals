80b31dac18311a6e1b33ec8cac55db658f8fd3f7
cao-awa <cao.awa.www@gmail.com>
2024-07-24 22:56:41 +0800

    Delete registry entry mixin.

--- src/main/java/com/github/cao/awa/sepals/registry/key/ReferenceLocatedRegistryKey.java
+++
@@ -1,9 +0,0 @@
-package com.github.cao.awa.sepals.registry.key;
-
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.tag.TagKey;
-
-public interface ReferenceLocatedRegistryKey<T> {
-    void sepals$setReference(RegistryEntry.Reference<TagKey<T>> reference);
-    boolean sepals$isIn(Object reference);
-}
